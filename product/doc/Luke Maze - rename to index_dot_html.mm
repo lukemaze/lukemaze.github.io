5e4f2892d1a2b0c15dd600d0808adefbcd4d07b7
lukemaze <73663381+lukemaze@users.noreply.github.com>
2020-11-03 19:50:35 -0700

    First Blog added

---
+++ product/doc/Luke Maze - rename to index_dot_html.mm
@@ -0,0 +1,109 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1604346596826" ID="ID_925147151" MODIFIED="1604365180135" TEXT="Luke Maze (@InnovateAndDo)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="28"/>
+<node COLOR="#0033ff" CREATED="1604364035425" FOLDED="true" ID="ID_1752108967" MODIFIED="1604365180133" POSITION="right" TEXT="Contact">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="kaddressbook"/>
+<node COLOR="#00b439" CREATED="1604362708112" ID="ID_727754996" LINK="mailto://lukemaze@gmail.com" MODIFIED="1604364074900" TEXT="E-Mail lukemaze@gmail.com">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604363310261" ID="ID_1798569094" LINK="tel://+16024928475" MODIFIED="1604364146864" TEXT="Phone +1 (602) 492-8475 ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604362810809" ID="ID_687095445" LINK="https://wa.me/+16024928475" MODIFIED="1604364121875" TEXT="WhatsApp +16024928475">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604362719569" ID="ID_1941780609" LINK="https://calendly.com/lukemaze" MODIFIED="1604364025271" TEXT="Schedule a meeting via Calendly.com">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1604346877488" FOLDED="true" ID="ID_240509705" MODIFIED="1604365180134" POSITION="right" TEXT="About">
+<edge COLOR="#990000" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="info"/>
+<node COLOR="#00b439" CREATED="1604350066053" ID="ID_1237357124" LINK="https://www.meinanutshell.com/lukemaze_mba#" MODIFIED="1604361647604" TEXT="Social Media Mashup">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604349988293" ID="ID_219472487" LINK="https://www.linkedin.com/in/lukemaze/" MODIFIED="1604361647604" TEXT="LinkedIn">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604351501063" ID="ID_1258701356" LINK="https://www.instagram.com/innovateanddo/" MODIFIED="1604361647605" TEXT="Instagram">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604349998988" ID="ID_1314547080" LINK="https://twitter.com/InnovateAndDo" MODIFIED="1604361647605" TEXT="Twitter">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604349991704" ID="ID_1732320292" LINK="https://www.facebook.com/r.luke.maze/" MODIFIED="1604361647605" TEXT="Facebook">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1604348256419" ID="ID_1396720515" MODIFIED="1604365120370" POSITION="right" TEXT="Powered by FreeMind | Copyright 2020">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="freemind_butterfly"/>
+</node>
+<node COLOR="#0033ff" CREATED="1604346851852" FOLDED="true" ID="ID_1899181288" MODIFIED="1604365180135" POSITION="left" TEXT="Product Innovation">
+<edge COLOR="#990000" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="launch"/>
+<node COLOR="#00b439" CREATED="1604348095868" ID="ID_1228422728" LINK="https://lukemaze.github.io/product" MODIFIED="1604361647610" TEXT="Product Blog">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604347305979" FOLDED="true" ID="ID_1627537365" MODIFIED="1604365180135" TEXT="AGoodListener.org">
+<edge COLOR="#990000" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1604347917451" ID="ID_1151388145" MODIFIED="1604361647616" TEXT="An AI ChatBot providing Gregorian talk therapy to the mental health industry">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1604347899595" ID="ID_906809019" LINK="http://AGoodListener.org" MODIFIED="1604361647628" TEXT="Text-only Minimum Viable Product (MVP)">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1604346893270" FOLDED="true" ID="ID_1291209520" MODIFIED="1604367398650" POSITION="left" TEXT="Lean-Agile Delivery">
+<edge COLOR="#990000" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="group"/>
+<node COLOR="#00b439" CREATED="1604348080861" ID="ID_1295826119" MODIFIED="1604361647608" TEXT="Agile Blog (coming soon)">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="calendar"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1604349951766" FOLDED="true" ID="ID_1279856023" MODIFIED="1604365180135" POSITION="left" TEXT="HG Consulting, LLC">
+<edge COLOR="#990000" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="wizard"/>
+<node COLOR="#00b439" CREATED="1604354825640" ID="ID_1647514256" MODIFIED="1604361647603" TEXT="Develop Lean-Agile leaders, empower self-organized innovation, and deliver business value better-faster-cheaper">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604349958536" ID="ID_1761874876" LINK="https://www.linkedin.com/company/hiredgun" MODIFIED="1604361647603" TEXT="LinkedIn">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604350525092" ID="ID_1116111147" LINK="https://ecorp.azcc.gov/BusinessSearch/BusinessInfo?entityNumber=L09762111" MODIFIED="1604361647603" TEXT="Arizona Corporation Commission (ACC)">
+<edge COLOR="#111111" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1604364258207" ID="ID_1517230671" LINK="mailto:hgconsulting@consultant.com" MODIFIED="1604364288995" TEXT="E-Mail HGConsulting@consultant.com">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
