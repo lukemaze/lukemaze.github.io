956f80926c39ac3dcafafca8ac286ae1364b1d86
lukemaze <73663381+lukemaze@users.noreply.github.com>
2020-11-08 21:58:34 -0700

    lukemaze

--- docs/REPLACE Expand and Collapse then RENAME to index.html.mm
+++ docs/REPLACE Expand and Collapse then RENAME to index.html.mm
@@ -1,11 +1,11 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#00b439" CREATED="1604346596826" ID="ID_925147151" MODIFIED="1604896864412" TEXT="Luke Maze (click &quot;+&quot; to expand)">
+<node COLOR="#00b439" CREATED="1604346596826" ID="ID_925147151" MODIFIED="1604897784302" TEXT="Luke Maze | InnovateAndDo&#xa;     (click &quot;+&quot; to expand) ">
 <edge COLOR="#00b439" WIDTH="2"/>
 <font BOLD="true" NAME="SansSerif" SIZE="20"/>
-<node COLOR="#990000" CREATED="1604346877488" FOLDED="true" ID="ID_240509705" MODIFIED="1604896864413" POSITION="right" TEXT="On the Web">
+<node COLOR="#990000" CREATED="1604346877488" FOLDED="true" ID="ID_240509705" MODIFIED="1604897827028" POSITION="right" TEXT="On the Web">
 <edge COLOR="#990000" WIDTH="1"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="SansSerif" SIZE="20"/>
 <icon BUILTIN="info"/>
 <node COLOR="#111111" CREATED="1604350066053" ID="ID_1237357124" LINK="https://www.meinanutshell.com/lukemaze_mba#" MODIFIED="1604896864413" TEXT="Social Media Mashup">
 <edge COLOR="#111111" WIDTH="thin"/>
@@ -28,12 +28,14 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#990000" CREATED="1604364035425" FOLDED="true" ID="ID_1752108967" MODIFIED="1604896864416" POSITION="right" TEXT="Contact">
+<node COLOR="#990000" CREATED="1604364035425" FOLDED="true" ID="ID_1752108967" MODIFIED="1604897828196" POSITION="right" TEXT="Contact">
 <edge COLOR="#990000" WIDTH="1"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="SansSerif" SIZE="20"/>
 <icon BUILTIN="kaddressbook"/>
-<node COLOR="#111111" CREATED="1604362708112" ID="ID_727754996" LINK="mailto://lukemaze@gmail.com" MODIFIED="1604896864416" TEXT="E-Mail lukemaze@gmail.com">
+<node COLOR="#111111" CREATED="1604362708112" ID="ID_727754996" LINK="mailto://lukemaze@innovateanddo.com" MODIFIED="1604897371051" TEXT="E-Mail LukeMaze@InnovateAndDo.com">
 <edge COLOR="#111111" WIDTH="thin"/>
+<arrowlink DESTINATION="ID_727754996" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1733114188" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_727754996" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1733114188" SOURCE="ID_727754996" STARTARROW="None" STARTINCLINATION="0;0;"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 <node COLOR="#111111" CREATED="1604363310261" ID="ID_1798569094" LINK="tel://+16024928475" MODIFIED="1604896864416" TEXT="Phone +1 (602) 492-8475 ">
@@ -49,18 +51,18 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#990000" CREATED="1604896675703" ID="ID_509836045" LINK="https://www.innovateanddo.com" MODIFIED="1604896864419" POSITION="right" TEXT="www.InnovateAndDo.com">
+<node COLOR="#990000" CREATED="1604896675703" ID="ID_509836045" LINK="https://www.innovateanddo.com" MODIFIED="1604897024619" POSITION="right" TEXT="InnovateAndDo.com">
 <edge COLOR="#990000" WIDTH="1"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="SansSerif" SIZE="20"/>
 </node>
 <node CREATED="1604348256419" ID="ID_1396720515" MODIFIED="1604896878164" POSITION="right" TEXT="Powered by FreeMind | Copyright 2020">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#990000" CREATED="1604346851852" FOLDED="true" ID="ID_1899181288" MODIFIED="1604896864421" POSITION="left" TEXT="Digital Product Innovation">
+<node COLOR="#990000" CREATED="1604346851852" FOLDED="true" ID="ID_1899181288" MODIFIED="1604897740673" POSITION="left" TEXT="Digital Product Innovation">
 <edge COLOR="#990000" WIDTH="1"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="SansSerif" SIZE="20"/>
 <icon BUILTIN="launch"/>
-<node COLOR="#111111" CREATED="1604348095868" ID="ID_1228422728" LINK="https://lukemaze.github.io/product" MODIFIED="1604896864421" TEXT="InnovateAndDo Blog - filter for &quot;Product Management&quot;">
+<node COLOR="#111111" CREATED="1604348095868" ID="ID_1228422728" LINK="https://lukemaze.github.io/blog" MODIFIED="1604897739180" TEXT="InnovateAndDo Blog - filter for &quot;Product Management&quot;">
 <edge COLOR="#111111" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
@@ -77,18 +79,18 @@
 </node>
 </node>
 </node>
-<node COLOR="#990000" CREATED="1604346851852" FOLDED="true" ID="ID_590355437" MODIFIED="1604896864424" POSITION="left" TEXT="Lean-Agile Value Delivery">
+<node COLOR="#990000" CREATED="1604346851852" FOLDED="true" ID="ID_590355437" MODIFIED="1604897762026" POSITION="left" TEXT="Lean-Agile Value Delivery">
 <edge COLOR="#990000" WIDTH="1"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="SansSerif" SIZE="20"/>
 <icon BUILTIN="group"/>
-<node COLOR="#111111" CREATED="1604348095868" ID="ID_1611201659" LINK="https://lukemaze.github.io/product" MODIFIED="1604896864424" TEXT="InnovateAndDo blog - filter for &quot;Agile Delivery&quot;">
+<node COLOR="#111111" CREATED="1604348095868" ID="ID_1611201659" LINK="https://lukemaze.github.io/blog" MODIFIED="1604897760532" TEXT="InnovateAndDo blog - filter for &quot;Agile Delivery&quot;">
 <edge COLOR="#111111" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#990000" CREATED="1604349951766" FOLDED="true" ID="ID_1279856023" MODIFIED="1604896864425" POSITION="left" TEXT="HG Consulting, LLC">
+<node COLOR="#990000" CREATED="1604349951766" FOLDED="true" ID="ID_1279856023" MODIFIED="1604897782162" POSITION="left" TEXT="HG Consulting, LLC">
 <edge COLOR="#990000" WIDTH="1"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="SansSerif" SIZE="20"/>
 <icon BUILTIN="wizard"/>
 <node COLOR="#111111" CREATED="1604354825640" ID="ID_1647514256" MODIFIED="1604896864425" TEXT="Develop Lean-Agile leaders, empower self-organized innovation, and deliver business value better-faster-cheaper">
 <edge COLOR="#111111" WIDTH="thin"/>
